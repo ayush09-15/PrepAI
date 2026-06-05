e0c597f2899d217bd9be42c72897c7ca36040ed7
ayush09-15 <ayushsingh15092002@gmail.com>
2026-06-06 00:54:23 +0530

    feat: protect frontend routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
+import ProtectedRoute from "./components/common/ProtectedRoute";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
@@ -14,8 +14,23 @@ function App() {
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path={ROUTES.LOGIN} element={<Login />} />
         <Route path={ROUTES.REGISTER} element={<Register />} />
-        <Route path={ROUTES.DASHBOARD} element={<Dashboard />} />
-        <Route path={ROUTES.INTERVIEW} element={<Interview />} />
+        <Route
+  path={ROUTES.DASHBOARD}
+  element={
+    <ProtectedRoute>
+      <Dashboard />
+    </ProtectedRoute>
+  }
+/>
+
+<Route
+  path={ROUTES.INTERVIEW}
+  element={
+    <ProtectedRoute>
+      <Interview />
+    </ProtectedRoute>
+  }
+/>
       </Routes>
     </BrowserRouter>
   );
