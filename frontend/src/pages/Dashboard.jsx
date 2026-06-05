e8e75e464857fa97b3d104c3be45e1a6f9c5fae8
ayush09-15 <ayushsingh15092002@gmail.com>
2026-06-06 00:59:20 +0530

    feat: implement logout functionality

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,28 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { AuthContext } from "../context/AuthContext";
+
 function Dashboard() {
-  return <h1>Dashboard Page</h1>;
+  const { logout } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+
+    navigate("/login");
+  };
+
+  return (
+    <div>
+      <h1>Dashboard Page</h1>
+
+      <button onClick={handleLogout}>
+        Logout
+      </button>
+    </div>
+  );
 }
 
 export default Dashboard;
