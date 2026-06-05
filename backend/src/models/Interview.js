de89bd89d0ebfb556562efd74c654864269e61be
ayush09-15 <ayushsingh15092002@gmail.com>
2026-06-06 01:13:15 +0530

    feat: create interview database model

---
+++ backend/src/models/Interview.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const interviewSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    role: {
+      type: String,
+      required: true,
+    },
+
+    difficulty: {
+      type: String,
+      enum: ["Easy", "Medium", "Hard"],
+      default: "Medium",
+    },
+
+    questions: [
+      {
+        type: String,
+      },
+    ],
+
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    status: {
+      type: String,
+      enum: ["Pending", "Completed"],
+      default: "Pending",
+    },
+
+    score: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model(
+  "Interview",
+  interviewSchema
+);
