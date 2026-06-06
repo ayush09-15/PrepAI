9ca60e8dde25060d6fdb8c4746576bc1eb7f6605
ayush09-15 <ayushsingh15092002@gmail.com>
2026-06-06 15:48:49 +0530

    feat: create interview session interface

--- frontend/src/pages/Interview.jsx
+++ frontend/src/pages/Interview.jsx
@@ -1,5 +1,193 @@
+import { useState } from "react";
+import { uploadResume } from
+"../services/api/interviewApi";
+
 function Interview() {
-  return <h1>Interview Page</h1>;
+  const [questions, setQuestions] =
+  useState([]);
+
+  const [started, setStarted] =
+  useState(false);
+
+const [currentQuestion,
+  setCurrentQuestion] =
+  useState(0);
+
+  const [formData, setFormData] =
+    useState({
+      title: "",
+      role: "",
+      difficulty: "Medium",
+    });
+
+  const [resume, setResume] =
+    useState(null);
+
+  const handleChange = (e) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.name]:
+        e.target.value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  try {
+    const uploadData =
+      new FormData();
+
+    uploadData.append(
+      "resume",
+      resume
+    );
+
+    const response =
+      await uploadResume(
+        uploadData
+      );
+
+    setQuestions(
+  response.interview.questions
+);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+  return (
+    <div>
+      <h1>Create Interview</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="title"
+          placeholder="Interview Title"
+          value={formData.title}
+          onChange={handleChange}
+        />
+
+        <br />
+        <br />
+
+        <input
+          type="text"
+          name="role"
+          placeholder="Role"
+          value={formData.role}
+          onChange={handleChange}
+        />
+
+        <br />
+        <br />
+
+        <select
+          name="difficulty"
+          value={
+            formData.difficulty
+          }
+          onChange={handleChange}
+        >
+          <option value="Easy">
+            Easy
+          </option>
+
+          <option value="Medium">
+            Medium
+          </option>
+
+          <option value="Hard">
+            Hard
+          </option>
+        </select>
+
+        <br />
+        <br />
+
+        <input
+          type="file"
+          accept=".pdf"
+          onChange={(e) =>
+            setResume(
+              e.target.files[0]
+            )
+          }
+        />
+
+        <br />
+        <br />
+
+        <button type="submit">
+          Create Interview
+        </button>
+      </form>
+      {questions.length > 0 && !started && (
+  <div>
+    <h2>Generated Questions</h2>
+
+    <ol>
+      {questions.map(
+        (question, index) => (
+          <li key={index}>
+            {question}
+          </li>
+        )
+      )}
+    </ol>
+
+    <button
+      onClick={() =>
+        setStarted(true)
+      }
+    >
+      Start Interview
+    </button>
+  </div>
+)}
+{started && (
+  <div>
+    <h2>
+      Question {currentQuestion + 1}
+    </h2>
+
+    <p>
+      {
+        questions[
+          currentQuestion
+        ]
+      }
+    </p>
+
+    <textarea
+      key={currentQuestion}
+      rows="5"
+      cols="50"
+      placeholder="Type your answer..."
+    />
+
+    <br />
+    <br />
+
+    <button
+      onClick={() =>
+        setCurrentQuestion(
+          (prev) => prev + 1
+        )
+      }
+      disabled={
+        currentQuestion ===
+        questions.length - 1
+      }
+    >
+      Next Question
+    </button>
+  </div>
+)}
+    </div>
+  );
 }
+
 
 export default Interview;
---
+++ frontend/src/services/api/interviewApi.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const API_URL =
+  "http://localhost:8000/api/interviews";
+
+export const uploadResume =
+  async (formData) => {
+    const token =
+      localStorage.getItem("token");
+
+    const response =
+      await axios.post(
+        `${API_URL}/upload-resume`,
+        formData,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+    return response.data;
+  };
